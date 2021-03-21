4a3c0a4a2c1c81d9aaf70f854d5625609809c5b9
damian.deleon <damianmdeleon@gmail.com>
2021-03-20 20:19:27 -0500

    updated code for posting

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# noteTaker
+# Note Taker
+## Table of Contents
+* [Description](##description)
+* [Installation](##installation)
+* [Usage](##usage)
+* [Questions](##questions)
+## Description
+This project will accept notes from the user and will save them.  
+![enter and save](https://user-images.githubusercontent.com/73486962/111890396-f442be00-89b6-11eb-9038-55e672dbf9de.png)
+
+They will also delete when the user clicks on the delete button.
+![delete note](https://user-images.githubusercontent.com/73486962/111890403-0886bb00-89b7-11eb-8939-f6c2c4b8d426.png)
+
+## Installation
+npm init -y, then npm install express.
+## Usage
+They would launch the app by entering "node server.js" in the terminal screen.  Then they would navigate to "http://localhost:3030" in their web browser.
+
+## Questions
+Please see my github | Contact me for questions
+------------ | -------------
+[Github Profile](https://github.com/damiandeleon) | damianmdeleon@gmail.com
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-let idNumber= 0;
+
 //Require Dependencies
 console.log("hi");
 const express = require('express');
@@ -26,55 +26,71 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 //html route to return the notes.html file
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+let idNumber = 1;
 
-//Get Existing Notes pulled from the server or db.json file
-app.get('/api/notes', (req, res) => {
-  //putting this in the function since putting the dependency as a global variable wouldn't work
-  const dataBase = require('./db.json');
-  res.send(dataBase);
-  // console.log(dataBase);
-  res.end();
-});
-
-// console.log(id);
-// Create and Post New Notes - takes in JSON input
-app.post('/api/notes', (req, res) => {
-  fs.readFile(__dirname + '/db.json', 'utf8', function (err, notes) {
-    if (err) throw err;
-    notes = JSON.parse(notes)
-    idNumber = idNumber + 1;
-    let templateNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: idNumber
-    }
-
-    let newNote = notes.concat(templateNote)
-    fs.writeFile(__dirname + "/db.json", JSON.stringify(newNote), (err, notes) => {
+function start() {
+  //Display the notes from json file
+  app.get('/api/notes', function (req, res) {
+    fs.readFile(__dirname + '/db/db.json', 'utf8', function (err, data) {
       if (err) throw err;
-      console.log(newNote)
-      res.json(newNote);
-      // res.end();
-
+      console.log("These are the saved notes", data)
+      res.json(JSON.parse(data))
     })
-    // console.log(id);
+  });
+
+  function increment(idNumber) {
+    idNumber++;
     return idNumber;
-  })
-})
-app.delete('/api/notes/:id', (req, res) => {
-  const noteId = JSON.parse(req.params.id)
-  console.log(noteId);
-  fs.readFile(__dirname + '/db.json', 'utf8', (err, notes) => {
-    if (err) throw err;
-    notes = JSON.parse(notes);
-    notes = notes.filter(val => val.id != noteId)
+  }
 
-    fs.writeFile(__dirname + '/db.json', JSON.stringify(notes), (err, data) => {
+  // console.log(id);
+  // Create and Post New Notes - takes in JSON input
+  app.post('/api/notes', (req, res) => {
+
+    increment(idNumber);
+    fs.readFile(__dirname + '/db/db.json', 'utf8', function (err, notes) {
       if (err) throw err;
-      res.json(notes)
+      notes = JSON.parse(notes)
+
+      let templateNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: (idNumber + 1)
+      }
+
+      // idNumber = notes.id + 1
+      // increment(idNumber);
+
+      let newNote = notes.concat(templateNote)
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(newNote), (err, data) => {
+        if (err) throw err;
+        console.log(newNote)
+        res.json(newNote);
+        // res.end();
+
+      })
+      // console.log(id);
+
     })
   })
-});
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = JSON.parse(req.params.id)
+    console.log(noteId);
+    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, notes) => {
+      if (err) throw err;
+      notes = JSON.parse(notes);
+      notes = notes.filter(val => val.id != noteId)
+
+      fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err, data) => {
+        if (err) throw err;
+        res.json(notes)
+      })
+    })
+  });
+}
+
 
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+start();
